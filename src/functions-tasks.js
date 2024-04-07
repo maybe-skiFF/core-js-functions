e3eecff1da5e871fbba8a8942b2f7cfc0729f5f8
maybe-skiFF <den.skiff190@gmail.com>
2024-04-07 18:46:20 +0300

    feat: add solution getIdGeneratorFunction task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -209,8 +209,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let idCounter = startFrom - 1;
+  return () => {
+    idCounter += 1;
+    return idCounter;
+  };
 }
 
 module.exports = {
