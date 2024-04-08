3245f36c33adb7d754a1011a81e97a14b683fafd
maybe-skiFF <den.skiff190@gmail.com>
2024-04-08 19:08:30 +0300

    feat: add solution logger task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -178,8 +178,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsToStr = JSON.stringify(args).slice(1, -1);
+    logFunc(`${func.name}(${argsToStr}) starts`);
+    const resLog = func(...args);
+    logFunc(`${func.name}(${argsToStr}) ends`);
+    return resLog;
+  };
 }
 
 /**
